6c42a081d5342807f447731d4adb6506bde399ae
Israel-199 <israelassefa199@gmail.com>
2025-09-30 01:07:16 -0700

    cron.js added

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js"
 import { connectDB } from './lib/db.js';
 import cors from "cors";
+import job from './lib/cron.js';
 
 const PORT=process.env.PORT || 3000;
 const app = express();
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
