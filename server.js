61482f04c4ab227c586110189055a194d6429623
Israel-199 <israelassefa199@gmail.com>
2025-09-30 00:20:43 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import "dotenv/config";
+import authRoutes from "./src/routes/authRoutes.js";
+import bookRoutes from "./src/routes/bookRoutes.js"
+import { connectDB } from './src/lib/db.js';
+import cors from "cors";
+
+const PORT=process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/books",bookRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+});
